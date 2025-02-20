e7ffe4681066d75cfd68754ea12b0d95e37c5d55
LeRodrigues2005 <leticiadepatta@gmail.com>
2025-02-20 19:46:10 -0300

    Code improvements

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from ascii_art import game_name
 game_name()
 
 def choose_language():
-    print("Choose the language:")
-    print("1 - English ")
-    print("2 - Portuguese (Brazil)")
+    while True:
+        print("Choose the language:")
+        print("1 - English")
+        print("2 - Portuguese (Brazil)")
 
-    choice = input("Option: ")
+        choice = input("Option: ")
 
-    if choice == "1":
-        subprocess.run(["python", "game_en/main_en.py"])
-    elif choice == "2":
-        subprocess.run(["python", "game_pt/main_pt.py"]) 
-    else:
-        print("Invalid option!")
-
+        if choice == "1":
+            # subprocess.run(["python", "game_en/main_en.py"])
+            print('\nOh, sorry... Language update is still in progress! Only Portuguese (2) is available for now ;)')
+            print('Try again!\n')
+        elif choice == "2":
+            subprocess.run(["python", "game_pt/main_pt.py"])
+            break
+        else:
+            print("Invalid option!\n")
 
 if __name__ == "__main__":
     choose_language()
+

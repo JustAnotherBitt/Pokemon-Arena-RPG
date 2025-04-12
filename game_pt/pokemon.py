958be57033f6d1aa288bdcd8602e4a6374f70402
LeRodrigues2005 <leticiadepatta@gmail.com>
2025-04-12 15:17:08 -0300

    Database synchronization and code update

--- game_pt/pokemon.py
+++ game_pt/pokemon.py
@@ -14,6 +14,29 @@ class Pokemon:  # Classe pai
 
     def __str__(self):
         return f'{self.especie} (Level {self.level})'
+    
+    def to_dict(self):
+        # Transforma o Pokémon em um dicionário (dados simples)
+        return {
+            'especie': self.especie,
+            'level': self.level,
+            'nome': self.nome,
+            'tipo': self.__class__.__name__  # Salva o tipo real do pokémon (ex: PokemonFogo)
+        }
+
+    @staticmethod
+    def from_dict(data):
+        # Importa as classes de Pokémon específicas
+        tipo = data.get('tipo')
+        
+        if tipo == 'PokemonFogo':
+            return PokemonFogo(data['especie'], data['level'], data['nome'])
+        elif tipo == 'PokemonAgua':
+            return PokemonAgua(data['especie'], data['level'], data['nome'])
+        elif tipo == 'PokemonEletrico':
+            return PokemonEletrico(data['especie'], data['level'], data['nome'])
+        else:
+            return Pokemon(data['especie'], data['level'], data['nome'])
 
     def atacar(self, pokemon):
         ataque_efetivo = int((self.ataque * random.random() * 1.3))
@@ -51,6 +74,6 @@ class PokemonAgua(Pokemon):  # Pokemon eletrico = classe filha do Pokemon
     tipo = "água"
 
     def atacar(self, pokemon):
-        print(f'{c.yellow}{self} lançou um jato de agua em {pokemon}{c.x}')
+        print(f'{c.yellow}{self} lançou um jato de água em {pokemon}{c.x}')
         sleep(1.5)
         return super().atacar(pokemon)
